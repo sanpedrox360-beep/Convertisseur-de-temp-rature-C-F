b8457b47a7ab5aad4d604130cee7696f3a729b37
San-Pédro ADJOH <sanpedrox360@gmail.com>
2026-08-21 04:17:47 +0000

    Initial commit

---
+++ conv.js
@@ -0,0 +1,37 @@
+// 1. Sélection des éléments HTML (avec 'document' en minuscules)
+const inputTemp = document.getElementById('inputTemp');
+const format = document.getElementById('convFormat');
+const affResultat = document.getElementById('resultat');
+// 2. Vos deux fonctions de conversion (très bien écrites !)
+function CelsiusToFahrenheit(temp) {
+    let convTemp = (temp * 9/5) + 32;
+    return convTemp;
+}
+
+function FahrenheitToCelsius(temp) {
+    let convTemp = (temp - 32) * 5/9;
+    return convTemp;
+}
+function calculer(temp){
+    let resultat;
+
+    if (format.value === "CelsiusToFahrenheit") {
+        resultat = CelsiusToFahrenheit(temp);
+        return `${temp} °C = ${resultat} °F`
+    } 
+    else {
+        resultat = FahrenheitToCelsius(temp);
+        return `${temp} °F = ${resultat} °C`
+    }
+}
+function afficher(){
+    if (inputTemp.value === ""){
+        inputTemp.value = 0;
+    }
+    affResultat.value = calculer(inputTemp.value);
+}
+inputTemp.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        afficher(); 
+    }
+});
